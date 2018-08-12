9bd22956cdb8ddc75251310984b9d4ab70b61e5b
malang <malang@DESKTOP-8KI72JM>
2018-08-12 15:37:32 +0700

    initial commit

---
+++ src/cobaoperasistring/CobaOperasiString.java
@@ -0,0 +1,13 @@
+
+package cobaoperasistring;
+
+
+public class CobaOperasiString {
+
+
+    public static void main(String[] args) {
+        String identitas = " Yosieka Putri Wibawa / X RPL 6 / 40";
+        System.out.println("identitas : " + identitas);
+    }
+    
+}
