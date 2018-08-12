b92165507dfcd5e1dc02912e968f412fdd4960d0
malang <malang@DESKTOP-8KI72JM>
2018-08-12 16:09:26 +0700

    coba method length & isEmpty

--- src/cobaoperasistring/CobaOperasiString.java
+++ src/cobaoperasistring/CobaOperasiString.java
@@ -8,6 +8,16 @@ public class CobaOperasiString {
     public static void main(String[] args) {
         String identitas = " Yosieka Putri Wibawa / X RPL 6 / 40";
         System.out.println("identitas : " + identitas);
+        
+        String x = "operasi";
+        System.out.println("isi variable x : " + x);
+        System.out.println("\"" + x + "\" panjang karakter = " + x.length());
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        String y = "";
+        System.out.println("isi variable y " + y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
+        
+        
     }
     
 }
